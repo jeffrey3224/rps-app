6c4a6b57ea2aebdcebc3bd8f1bc35f6c8789afe6
Jeffrey Girard <jeffrey3224@gmail.com>
2026-03-10 22:14:22 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Rules from './components/Rules'
+
+function App() {
+
+  return (
+    <main className='w-[100vw] h-screen'>
+      <Rules />
+    </main>
+    
+  )
+}
+
+export default App
---
+++ src/components/Rules.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+export default function Rules() {
+
+    const [showModal, setShowModal] = useState(true);
+
+    const handleClose = () => {
+        setShowModal(false);
+    }
+
+    return (
+        showModal && 
+            (   <div className="w-full h-screen flex justify-center items-center bg-black bg/75">
+                    <div className="bg-white p-10 rounded-2xl flex flex-col justify-end relative">
+                        <h2 className="text-black text-3xl">RULES</h2>
+                        <img src="/images/image-rules.svg" className="w-[25vw]" />
+                        <button onClick={handleClose} className="absolute top-5 right-5">
+                            <img src="/images/icon-close.svg" width={22} />
+                        </button>
+                        
+                    </div>
+                </div>
+            )
+    )
+}
