3ff5e4032f664b6303d82d956808b6e8fb1d226c
Jeffrey Girard <jeffrey3224@gmail.com>
2026-03-21 09:35:48 -0700

    fixed height constraint on rules overlay

--- src/components/Bonus/BonusRules.tsx
+++ src/components/Bonus/BonusRules.tsx
@@ -12,7 +12,7 @@ export default function BonusRules({onSelect, showRules}: Props) {
 
     return (
         showRules && 
-            (   <div className="z-100 absolute top-0 left-0 w-[100vw] h-[100vh] flex justify-center items-center bg-black/50">
+            (   <div className="z-100 absolute top-0 left-0 w-full h-full flex justify-center items-center bg-black/50">
                     <div className="bg-white p-10 rounded-2xl flex flex-col justify-end relative">
                         <h2 className="text-[rgb(137,131,142)] text-3xl font-bold mb-10">RULES</h2>
                         <img src="/images/image-rules-bonus.svg" className="w-[60vw] md:w-[25vw] min-w-[200px] max-w-[400px]" />
